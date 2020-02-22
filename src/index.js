69432294208dde2b39601fd9226d09f167cca066
artytrik <tema-luch@mail.ru>
2020-02-22 22:35:45 +0300

    Инициилизирует новое хранилище

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 import {Settings, questions} from './mocks/questions.js';
+import {createStore} from 'redux';
+import {reducer} from './reducer.js';
+import {Provider} from 'react-redux';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <App
-      errorsCount={Settings.ERRORS_COUNT}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        errorsCount={Settings.ERRORS_COUNT}
+        questions={questions}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
