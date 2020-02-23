db6cf5aab950c67f07171a218bd955c074ed7b34
artytrik <tema-luch@mail.ru>
2020-02-23 13:19:11 +0300

    Делает тесты для редьюсера

---
+++ src/components/game-screen/game-screen.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {GameScreen} from './game-screen.jsx';
+import {GameType} from '../../utils';
+
+const children = <div className="children-component" />;
+
+describe(`GameScreen should render correctly`, () => {
+  it(`with type GameType.ARTIST`, () => {
+    const tree = renderer.create(
+        <GameScreen
+          type={GameType.ARTIST}
+          mistakes={3}
+        >
+          {children}
+        </GameScreen>
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`with type GameType.GENRE`, () => {
+    const tree = renderer.create(
+        <GameScreen
+          type={GameType.GENRE}
+          mistakes={3}
+        >
+          {children}
+        </GameScreen>
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
