d90f42f9ba734d672d307452f31382c60e647ddb
artytrik <tema-luch@mail.ru>
2020-02-29 13:52:27 +0300

    Делает компонент отдельной песни в экране выбора жанра для увеличения производительности

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {GameType} from '../../utils.js';
+import GenreQuestionItem from '../genre-question-item/genre-question-item.jsx';
 
 class GenreQuestionScreen extends PureComponent {
   render() {
@@ -23,20 +24,14 @@ class GenreQuestionScreen extends PureComponent {
           }}
         >
           {answers.map((answer, i) => (
-            <div key={`${i}-${answer.src}`} className="track">
-              {renderPlayer(answer.src, i)}
-              <div className="game__answer">
-                <input className="game__input visually-hidden" type="checkbox"
-                  name="answer" value={`answer-${i}`} id={`answer-${i}`}
-                  checked={userAnswers[i]}
-                  onChange={(evt) => {
-                    const value = evt.target.checked;
-                    onChange(i, value);
-                  }}
-                />
-                <label className="game__check" htmlFor={`answer-${i}`}>Отметить</label>
-              </div>
-            </div>
+            <GenreQuestionItem
+              answer={answer}
+              id={i}
+              key={`${i}-${answer.src}`}
+              onChange={onChange}
+              renderPlayer={renderPlayer}
+              userAnswer={userAnswers[i]}
+            />
           ))}
           <button className="game__submit button" type="submit">Ответить</button>
         </form>
