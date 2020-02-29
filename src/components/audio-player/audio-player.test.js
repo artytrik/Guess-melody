0cea189fa9bb7a2be90aa5c7f6ce3a6eb4351b11
artytrik <tema-luch@mail.ru>
2020-02-29 10:28:30 +0300

    Делает тест для хока аудио

--- src/components/audio-player/audio-player.test.js
+++ src/components/audio-player/audio-player.test.js
@@ -17,7 +17,9 @@ it(`AudioPlayer is rendered correctly`, () => {
       isLoading={true}
       src={song.src}
       onPlayButtonClick={() => {}}
-    />
+    >
+      <audio />
+    </AudioPlayer>
   ), {
     createNodeMock: () => {
       return {};
